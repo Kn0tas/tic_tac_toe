9ef0db4a870a232a79cb020dcfbaa57ae0eda84f
Kn0tas <linusekvall89@gmail.com>
2023-01-31 21:53:24 +0100

    Make the tests more human readable

--- tests/test_TicTacToe.py
+++ tests/test_TicTacToe.py
@@ -11,17 +11,23 @@ class TestTicTacToe(unittest.TestCase):
 
     def test_horizontal_win(self):
         game = TicTacToe()
-        game.board = [[1, 1, 1], [0, 0, 0], [0, 0, 0]]
+        game.board = [[1, 1, 1],
+                      [0, 0, 0],
+                      [0, 0, 0]]
         self.assertTrue(game.check_winner(), "Expected horizontal win")
         
     def test_vertical_win(self):
         game = TicTacToe()
-        game.board = [[1, 0, 0], [1, 0, 0], [1, 0, 0]]
+        game.board = [[1, 0, 0],
+                      [1, 0, 0],
+                      [1, 0, 0]]
         self.assertTrue(game.check_winner(), "Expected vertical win")
         
     def test_diagonal_win(self):
         game = TicTacToe()
-        game.board = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        game.board = [[1, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 1]]
         self.assertTrue(game.check_winner(), "Expected diagonal win")
 
 if __name__ == '__main__':
